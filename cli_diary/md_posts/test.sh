65957fe748dbfd09b2ef66319aa80ac929d83346
mclds <mclds@protonmail.com>
2023-03-07 00:00:21 +0000

    07-03-2023 commit

---
+++ cli_diary/md_posts/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : mclds (mclds@protonmail.com)
+# @file        : test
+# @created     : Monday Mar 06, 2023 02:00:11 WET
+#
+# @description : 
+######################################################################
+
+BROWSER=$(echo$BROWSER|sed-e's|:xdg-open||g'-e's|xdg-open:||g')
+
